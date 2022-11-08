3c099978e98593332a00e1c537a0c02b827b9229
PedroSilvaBarros <pedrinhob2011@hotmail.com>
2022-11-08 11:34:54 +0000

    atualizando o front

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -16,31 +16,6 @@ export default function Home() {
       <iframe className = {"iframe"} src=" http://webcaseiot.com.br:1880/ui/#!/0?socketid=2rHrz1OO3n-pw8QfAAFr">
       </iframe>
       </div>
-      <div className={"content-block"}>
-        <div className={"dx-card responsive-paddings"}>
-          <div className="buttons">
-            <button className="button-one">Sensor 1</button>
-              <Switch defaultValue={true} />
-          </div>
-          <div className="buttons">
-            <button className="button-one">Sensor 1</button>
-            <Switch defaultValue={true} />
-          </div>
-          
-          <div className="buttons">
-            <button className="button-one">Sensor 1</button>
-            <Switch defaultValue={true} />
-          </div>
-          <div className="buttons">
-            <button className="button-one">Sensor 1</button>
-            <Switch defaultValue={true} />
-          </div>
-          <div className="buttons">
-            <button className="button-one">Atualizar Arduíno</button>
-          </div>
-          
-        </div>
-      </div>
     </React.Fragment>
   );
 }
